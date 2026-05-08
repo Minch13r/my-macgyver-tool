1ce02ea7c4ab4d1e2d6fafd9f36bd06f1afb6b5a
최민영 <128110770+Minch13r@users.noreply.github.com>
2026-05-08 22:31:46 +0900

    언어 설정

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
+import { redirect } from "react-router";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { DICTIONARY, DEFAULT_LANG } from "../constants/dictionary";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export function loader({ request }: Route.LoaderArgs) {
+  // 브라우저 언어 확인 및 기본값 설정
+  const acceptLanguage = request.headers.get("Accept-Language") || DEFAULT_LANG;
+  const preferredLang = acceptLanguage.split(",")[0].split("-")[0];
+  
+  const supportedLangs = Object.keys(DICTIONARY);
+  
+  // 지원하는 언어면 해당 언어로, 아니면 기본값(en)으로 설정
+  const finalLang = supportedLangs.includes(preferredLang) ? preferredLang : DEFAULT_LANG;
+
+  return redirect(`/${finalLang}`);
 }
 
 export default function Home() {
-  return <Welcome />;
+  return null;
 }
